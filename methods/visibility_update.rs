8c940a34d717e8f080059051d5e96538526566ff
Louis Birla <louis.birla@gmail.com>
2021-06-12 22:04:58 -0500

    fix: update visibility of document text as well

--- methods/visibility_update.rs
+++ methods/visibility_update.rs
@@ -9,10 +9,16 @@ impl DocumentBlock {
 		public: bool,
 	) -> Result<(), LoopError> {
 		let conn = &context.conn()?;
-		let Self { name, .. } = Self::from_id_admin(block_id, conn)?;
+		let Self { name, items } = Self::from_id_admin(block_id, conn)?;
 
 		if let Some(name) = name {
 			name.update_public(public, conn)?;
+		}
+
+		for item in items {
+			if item.block_type == "text" {
+				item.update_public(public, conn)?;
+			}
 		}
 
 		Ok(())
